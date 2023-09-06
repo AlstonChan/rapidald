d737cbb4ebe75901943323bfe59ff3efa86d7073
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2023-09-06 08:32:29 +0800

    Fix incorrect pricing & error cause infinite loop

--- menu_food_selection.cpp
+++ menu_food_selection.cpp
@@ -57,10 +57,14 @@ void food_selection(int option_num) {
 			cout << "Select a drinks: ";
 			cin >> drinks_selection;
 
-			handleInvalid("Select a drinks: ", drinks_selection);
-
-			while(drinks_selection < 1 && drinks_selection > 3)
+			while (cin.fail()) {
 				handleInvalid("Select a drinks: ", drinks_selection);
+			}
+
+			// using handleInvalid cause infinite loop
+			while (drinks_selection < 1 || drinks_selection > 3) {
+				handleInvalid("Select a drinks :", drinks_selection);
+			}
 
 			cout << "\nDrinks selected: \n" << menu_list_item[drinks_selection + 9][0] << "\n\n";
 
@@ -70,10 +74,15 @@ void food_selection(int option_num) {
 			cout << "Select a snacks: ";
 			cin >> snacks_selection;
 
-			handleInvalid("Select a snacks: ", snacks_selection);
-
-			while (snacks_selection < 1 && snacks_selection > 2)
+			while (cin.fail()) {
 				handleInvalid("Select a snacks: ", snacks_selection);
+			}
+
+			while (snacks_selection < 1 || snacks_selection > 2) {
+				handleInvalid("Select a snacks :", snacks_selection);
+			}
+
+			cout << snacks_selection;
 
 			cout << "\nSnacks selected: \n" << menu_list_item[snacks_selection + 13][0] << "\n";
 		}
@@ -91,7 +100,7 @@ void food_selection(int option_num) {
 	}
 	cout << "Quantity : " << order_quantity << " item\n";
 	if (option_num >= 1 && option_num <= 9) {
-		cout << "Price: RM " << (order_quantity * stol(menu_list_item[menu_id][2]) + 6);
+		cout << "Price: RM " << (order_quantity * (stol(menu_list_item[menu_id][2]) + 6));
 	} else
 		cout << "Price: RM " << (order_quantity * stol(menu_list_item[menu_id][2]));
 	cout << "\n\n----------Options----------\n";
@@ -102,11 +111,7 @@ void food_selection(int option_num) {
 	cin >> user_options;
 
 	while (cin.fail() || (user_options < 0 || user_options >= 2)) {
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Invalid input, Please try again.";
-		cout << "\nEnter your input : ";
-		cin >> user_options;
+		handleInvalid("Enter your input : ", user_options);
 	}
 
 	if (user_options == 0) {
@@ -120,15 +125,15 @@ void food_selection_quantity(int &order_quantity) {
 	cout << "\nQuantity : ";
 	cin >> order_quantity;
 
-	handleInvalid("Quantity : ", order_quantity);
+	while (cin.fail()) {
+		handleInvalid("Quantity : ", order_quantity);
+	}
 }
 
 void handleInvalid(string text, int &input) {
-	while (cin.fail()) {
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		cout << "Invalid input, Please try again.";
 		cout << "\n" << text;
 		cin >> input;
-	}
 }
